84a91e871455d768e8cf04aa1b811fcdf674f7eb
laurenp305 <lamor1800@gmail.com>
2022-08-06 12:01:35 -0400

    added git ignore file

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,18 +7,4 @@ var searchButton = document.querySelector('.btn');
 
 const APIKey = "84b79da5e5d7c92085660485702f4ce8";
 
-searchButton.addEventListener('click', function(name){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=84b79da5e5d7c92085660485702f4ce8')
-    .then(response => response.json())
-    .then(data => {
-      var temperatureValue = data['cityName']['temperature'];
-      var cityNameValue = data['cityName'];
-      var descriptionValue = data['weather'][0]['description'];
-    
-      cityName.innerHTML = nameValue;
-      description.innerHTML = "Description - "+descriptionValue;
-      temperature.innerHTML = "Temperature - "+temperatureValue;
-      search.value ="";
-    
-    })
-    
+//https://api.openweathermap.org/data/2.5/weather
